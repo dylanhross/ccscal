2432cced970af1fb7b58483ee8486e2eed1db57e
dylanhross <DylanRoss@Dylans-MacBook-Pro.local>
2016-06-10 11:06:28 -0700

    finished implementation of ParseInputFile
    Finished implementing the ParseInputFile class in the main execution
    block. Also, made some minor/cosmetic changes here and there

--- CcsCal.py
+++ CcsCal.py
@@ -96,7 +96,9 @@ class GetData (object):
 	def callPreProcessTxt(self, data_filename, specified_mass, mass_window):
 		# pre-process data with rough mass_window (i.e. double the original mass window)
 		useWindow = 2.0 * mass_window
-		# TODO: find a better way of specifying the PreProcessTxt.o executable location 
+		# TODO: find a better way of specifying the PreProcessTxt.o executable location (otherwise
+		#		it screws up the portability of the program, plus it needs to change to .exe when
+		#		it's compiled on windows
 		functionCallLine = "/Users/DylanRoss/ccscal-plusplus/PreProcessTxt.o" + " " +\
 							data_filename + " " +\
 							str(specified_mass) + " " + \
@@ -219,7 +221,7 @@ class GaussFit (object):
 			self.opt_mean = self.initparams[1]
 			# if fit was not achieved..
 			self.optparams = self.initparams
-                        print "failed to fit gaussian for mass", self.mass, "in", self.filename
+            print "failed to fit gaussian for mass", self.mass, "in", self.filename
 			
 	"""
 		GaussFit.saveGaussFitFig -- Method
@@ -677,6 +679,7 @@ class ParseInputFile (object):
 		ParseInputFile -- Class
 		
 		TODO: class description
+		TODO: data stored section
 		
 		Input(s):
 			input_filename				- name (and path) of CcsCalInput.txt file (string)
@@ -834,15 +837,11 @@ if __name__ == '__main__' :
 								 input_data.calibrantData[1],\
 								 mass_window=input_data.massWindow,\
 								 edc=input_data.edc)
-
 	# save a graph of the fitted calibration curve
 	calibration.saveCalCurveFig(figure_file_name=input_data.calCurveFileName)
 	# write the calibration statistics to the report file
 	report.writeCalibrationReport(calibration)
 	print "...DONE"
-	
-	
-
 	#
 	### EXTRACT DRIFT TIMES OF COMPOUNDS AND GET THEIR CALIBRATED CCS
 	#
@@ -851,23 +850,22 @@ if __name__ == '__main__' :
 	# write the header for the compound data table in the report
 	report.writeCompoundDataTableHeader()
 	# cycle through each compound input filename/mass pair and perform drift time extraction
-	
-	# TODO: fix this part... need to use the data arrays in the ParseInputFile object
-	
-	count = 0
-	for pair in (ccscal_input.compound_data_files_and_masses):
-		count += 1
-		print "Extracting Drift Time for Mass:", pair[1], "from Data File:", pair[0],\
-			  "(" + str(count), "of", str(len(ccscal_input.compound_data_files_and_masses)) + ")..."
+	for n in range(len(input_data.compoundData[0])):
+		print "Extracting Drift Time for Mass:", input_data.compoundData[1][n], \
+				"from Data File:", input_data.compoundData[0][n], "(" + str(n + 1), \
+				"of", str(len(input_data.compoundData[0])) + ")..."
 		# extract drift time and get calibrated CCS for the filename/mass pair
-		driftTime = collector.process((ccscal_input.compound_root_directory + pair[0]), pair[1], ccscal_input.mass_window)
+		driftTime = collector.process((input_data.compoundDataDir + input_data.compoundData[0][n]), \
+										input_data.compoundData[1][n], \
+										input_data.massWindow)
 		print "...DONE"
 		print "Getting Calibrated CCS..."
-		ccs =  calibration.getCalibratedCcs(pair[1], driftTime)
-		report.writeCompoundDataTableLine(pair[0], pair[1], driftTime, ccs)
+		ccs =  calibration.getCalibratedCcs(input_data.compoundData[1][n], driftTime)
+		report.writeCompoundDataTableLine(input_data.compoundData[0][n], \
+											input_data.compoundData[1][n], \
+											driftTime, \
+											ccs)
 		print "...DONE"
-	
-
 	#
 	### CLOSE THE REPORT FILE
 	report.finish()
@@ -876,7 +874,8 @@ if __name__ == '__main__' :
 	print "CcsCal Complete."
 	#
 	### COMPLETE
-
+	#
+	# report the total time taken to the user
 	end_time = time.time()
 	print ""
 	print "total time: ", (end_time - start_time)
